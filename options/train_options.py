84be18d6b179f1af38d4efe755071414fdebc960
anassbellache <bel_ans@hotmail.com>
2020-12-29 14:14:19 +0100

    dataset filepath

--- options/train_options.py
+++ options/train_options.py
@@ -25,10 +25,12 @@ class TrainOptions(BaseOptions):
         self.ytrain = config['dataset']['ytrain']
         self.xtest = config['dataset']['xtest']
         self.ytest = config['dataset']['ytest']
+        self.filename = config['dataset']['filename']
         self.lr_policy = config['training']['lr_policy']
         self.continue_train = config['training']['continue_train']
         self.source_distance = config['setup']['source_distance']
         self.det_distance = config['setup']['det_distance']
         self.det_count = config['setup']['det_count']
         self.det_spacing = config['setup']['det_spacing']
+        self.parellel = config['setup']['parallel']
         self.isTrain = True
